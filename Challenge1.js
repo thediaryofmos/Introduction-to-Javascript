16b20c8368592a40b8d813aa81302fadf16f4c5a
Muhammad Osama Shahzad <usamashahzad19@gmail.com>
2020-10-08 01:45:57 +0500

    Created if-else Challenge

---
+++ Challenge1.js
@@ -0,0 +1,20 @@
+function challenge(num){
+    if (num < 5){
+        return "Tiny";
+    }
+    else if (num < 10){
+        return "Small";
+    }
+    else if (num < 15){
+        return "Medium";
+    }
+    else if (num < 20){
+        return "Large";
+    }
+    else {
+        return "Huge";
+    }
+   
+}
+
+console.log(challenge(20));
