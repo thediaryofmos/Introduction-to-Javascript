90eff5473fe53541525e1b21724f84c3d9e0724a
Muhammad Osama Shahzad <usamashahzad19@gmail.com>
2020-10-07 16:32:29 +0500

    Added Elseif, if-else statements and AND, OR Gate

--- File3.js
+++ File3.js
@@ -40,4 +40,38 @@ function testGreater(val, b){
     }
     return "Less";
 }
-console.log(testGreater(10, "11"));
+//console.log(testGreater(10, "11"));
+
+//AND Operator
+function testGreater(val, b){
+    if (val >= b && val <50){
+        return "Greater than B but less than 50";
+    }
+    return "Condition Failed";
+}
+//console.log(testGreater(50, "5"));
+
+//OR Operator
+function testGreater(val){
+    if (val >= 100 || val <50){
+        return "Condition fulfilled";
+    }
+    return "Condition Failed";
+}
+//console.log(testGreater(100));
+
+//Else Statement and elseif statement
+function testGreater(val){
+    if (val >= 100 || val <50){
+        return "Condition fulfilled";
+    }
+    
+    else if (val == 60) {
+        return "Safe Number pressed"
+    }
+
+    else {
+        return "Condition Failed";
+    }
+}
+console.log(testGreater(60));
