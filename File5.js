f7f3c847658a6d6499f73cdec80026e6406aae3b
Muhammad Osama Shahzad <usamashahzad19@gmail.com>
2020-10-21 00:24:11 +0500

    Modified File5.js and added Tenery Operators etc

--- File5.js
+++ File5.js
@@ -35,3 +35,70 @@ function multiply(arr){
 }
 answer = multiply([[1,2], [1,4,8], [1,5,9,7]]);
 //console.log(answer);
+
+
+//do while Loop (First runs then check the condition at th end)
+var myArray = []
+var i = 0;
+do {
+    myArray.push(i);
+    i++;
+    //console.log(myArray);
+} while (i<5)
+
+
+//Generate Random
+function random(){
+    return Math.random();
+}
+//console.log(random());
+var RandomBetween0and19 = Math.floor(Math.random() * 20); //Random Whole Numbers 
+
+//Random Whole Numbers Between a specific range
+function random(max, min){
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+//console.log(random(50, 15));
+
+//Parseint Function converts str to integer
+function parsey(str){
+    return parseInt(str);
+}
+//console.log(parsey("500"));
+
+
+//parseInt converting binary string to integer
+function parsey(str){
+    return parseInt(str, 2);
+}
+//console.log(parsey("001001101001"));
+
+//Ternary Oprator
+//condition ? statment-if-True: statement-if-false
+function chkEqual(a,b){
+    return(a==b)? true : false;
+}
+//chkEqual(5, 5);
+
+//Multiple Conditions Ternary Operator
+function chkSign(num){
+    return num > 0 ? "positive": num < 0 ? "negative": "zero";
+}
+//console.log(chkSign(0))
+
+
+//
+//Difference btween let and var keyword
+//
+var cat = "Meow";
+var cat = "Purr"
+//var can be declared twice
+let cat1 = 1;
+//let cat1 = 2; //declaring with let twice will give an error
+cat1 = 4; //This will not give an error, onl re-declaring will
+"use strict"; //It will catch common coding mistakes
+//var is declard globally or in whole function if declared inside a function
+//while
+// lt is only limitd to th block-scope (i.e if let is initialized inside an if-statement,
+// that variable value will only exist in if-statement)
+
